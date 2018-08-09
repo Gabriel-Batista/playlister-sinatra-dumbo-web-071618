74e85c0c20185cda9d32f8f03c5cda6c81eb9e43
Tokuhisa1 <TokuhisaWinston@Outlook.com>
2018-08-09 11:53:41 -0400

    Corrects relationships between models

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,18 @@
 class SongController < ApplicationController
 
   get '/songs/new' do
+    @genres = Genre.all
     erb :'song/new'
   end
 
+  get '/songs/:id' do
+    'Hello World!'
+  end
+
+  post '/songs' do
+    new_song = Song.new(params[:name])
+    artitst = Artist.find_or_create_by(name: params[:artist])
+    genre = Genre.find_or_create_by(id: params[:genre])
+    redirect to "/songs/#{new_song.id}"
+  end
 end
